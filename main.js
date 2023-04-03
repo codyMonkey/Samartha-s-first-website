6d2d4aa8aeb24d005074e36bd3a88f1600564598
codyMonkey <92737380+codyMonkey@users.noreply.github.com>
2023-04-03 14:50:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function surname()
+{
+    document.getElementById("siri").innerHTML = "samartha pk";
+}
+
+function hide_surname()
+{
+    document.getElementById("siri").innerHTML = "samartha";
+}
+
+function history()
+{
+    document.getElementById("sam").innerHTML = "samartha is a good boy.he is studying in 8th standard good bye !"
+    byes()
+}
+
+function hide_history()
+{
+    document.getElementById("sam").innerHTML = ""
+}
+
+function byes()
+{
+    document.getElementById("bye").innerHTML = '"Thankyou for"<br>"visiting our website"'
+}
